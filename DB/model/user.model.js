e05cad61a4334eeb7879ec797f0d860a7133752e
amaged1896 <ahmed.maged1896@gmail.com>
2023-09-23 04:03:25 +0300

    "user model"

---
+++ DB/model/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        min: [3, 'min length 3 characters'],
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female']
+    },
+    phone: String,
+    status: {
+        type: String,
+        enum: ['online', 'offline'],
+        default: 'offline',
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    forgetCode: String,
+    activationCode: String,
+    profileImage: {
+        url: {
+            type: String,
+            default: "https://res.cloudinary.com/dyrznlkg1/image/upload/v1695430784/ecommerceDefaults/user/149071_fm67fh.png"
+        },
+        id: {
+            type: String,
+            default: "ecommerceDefaults/user/149071_fm67fh"
+        }
+    },
+    coverImages: [{
+        url: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        }
+    }]
+}, { timestamps: true });
+
+export const userModel = mongoose.model('user', userSchema);
