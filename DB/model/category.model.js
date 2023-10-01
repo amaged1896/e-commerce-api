53d714978dec6fdd4701f96426713eea8f6d9371
amaged1896 <ahmed.maged1896@gmail.com>
2023-10-01 08:31:18 +0300

    "category model"

---
+++ DB/model/category.model.js
@@ -0,0 +1,30 @@
+import mongoose, { Types } from "mongoose";
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        min: [4, 'min length is 4 characters'],
+        max: [4, 'max length is 15 characters'],
+        required: true,
+    },
+    slug: {
+        type: String,
+        required: true,
+    },
+    image: {
+        url: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        }
+    },
+    createdBy: {
+        type: Types.ObjectId,
+        ref: 'user',
+        required: true
+    }
+}, { timestamps: true });
+
+export const categoryModel = mongoose.model('category', categorySchema);
