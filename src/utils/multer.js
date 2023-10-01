33fb2d31db64dc7f600aeeaf194b014d34f58513
amaged1896 <ahmed.maged1896@gmail.com>
2023-10-01 06:53:33 +0300

    "file upload (multer)"

---
+++ src/utils/multer.js
@@ -0,0 +1,18 @@
+import multer, { diskStorage } from "multer";
+import { AppError } from "./appError.js";
+
+export const filterObject = {
+    image: ['image/png', 'image/jpeg'],
+    pdf: ['application/pdf'],
+    video: ['video/mp4']
+};
+
+export const fileUpload = (filterArray) => {
+    const fileFilter = (req, file, cb) => {
+        if (!filterArray.includes(file.mimetype)) {
+            return cb(new AppError("Invalid file format!"), false);
+        }
+        return cb(null, true);
+    };
+    return multer({ storage: diskStorage({}), fileFilter });
+};
